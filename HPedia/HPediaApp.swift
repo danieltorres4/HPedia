9370af2af9f5cea023da49606fcf9c0336d1a7f4
danieltorres4 <dst990502@outlook.com>
2023-03-22 13:04:24 -0600

    Initial Commit

---
+++ HPedia/HPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPediaApp.swift
+//  HPedia
+//
+//  Created by Iván Sánchez Torres on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
